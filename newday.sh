1865326ac09ac4a55cf048f9e67e712d2e1ca64e
Kane York <kanepyork@gmail.com>
2025-01-21 10:34:58 -0800

    add autocommit to newday.sh

--- newday.sh
+++ newday.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+git diff --cached --exit-code || (echo Dirty git staging!; exit 1)
+
 echo Creating day $1
 
 folder="day$1"
@@ -9,5 +11,6 @@ cargo init --bin $folder
 cp template/src/main.rs $folder/src/main.rs
 cd $folder
 cargo add anyhow
-
+git add .
+git commit -m "Initial create $folder"
 
